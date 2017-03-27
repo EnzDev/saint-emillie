a5cc080a5ce5693bdafb21084831dd2b9b1748b2
Nicolas RICHARD <nicolas.richard1@etu.univ-nantes.fr>
2017-03-27 19:25:56 +0200

    script formulaire

--- Html/script.js
+++ Html/script.js
@@ -3,3 +3,15 @@ $('a').click(function(){
     scrollTop: $( $(this).attr('href') ).offset().top
   }, 750);
 });
+$("#button").click(function(){
+  $("#form").css("display","block");
+  $("#formbg").css("display","block");
+});
+$("#annuler").click(function(){
+  $("#form").css("display","none");
+  $("#formbg").css("display","none");
+});
+$("#formbg").click(function(){
+  $("#form").css("display","none");
+  $("#formbg").css("display","none");
+});
