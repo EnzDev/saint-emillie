c3cdad5f5cdc56fb277a208a9d6a9d71343b2f99
Enzdev <enzo@mallard.ml>
2017-03-28 10:11:29 +0200

    message

--- Html/script.js
+++ Html/script.js
@@ -1,25 +1,26 @@
+$("#formulaire").load("formulaire.html", function(){
+  $("#button").click(function(){
+    $("#formContainer").css("display","block");
+    $("#formbg").css("display","block");
+  });
+  $("#annuler").click(function(){
+    $("#formContainer").css("display","none");
+    $("#formbg").css("display","none");
+  });
+  $("#envoyer").click(function(){
+    $("#formContainer").css("display","none");
+    $("#formbg").css("display","none");
+    alert("Donénes enregistrées !");
+  });
+  $("#formbg").click(function(){
+    $("#formContainer").css("display","none");
+    $("#formbg").css("display","none");
+
+  });
+});
+
 $('a').click(function(){
   $('html, body').animate({
     scrollTop: $( $(this).attr('href') ).offset().top
   }, 750);
 });
-$("#button").click(function(){
-  $("#formContainer").css("display","block");
-  $("#formbg").css("display","block");
-});
-$("#annuler").click(function(){
-  $("#formContainer").css("display","none");
-  $("#formbg").css("display","none");
-});
-$("#envoyer").click(function(){
-  $("#formContainer").css("display","none");
-  $("#formbg").css("display","none");
-  alert("Donénes enregistrées !");
-});
-$("#formbg").click(function(){
-  $("#formContainer").css("display","none");
-  $("#formbg").css("display","none");
-
-});
-
-$("#formulaire").load("formulaire.html");
